b31966fca50be7cbb0d2fbd58fcfd0f2ba7eb6a2
Alex <alexsouzasilvax@gmail.com>
2020-07-04 22:38:34 -0300

    inclusao novas acoes

--- src/main/java/br/com/loja/produto/Controller/ProdutoController.java
+++ src/main/java/br/com/loja/produto/Controller/ProdutoController.java
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,14 +24,6 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository produtoRepository;
 
-    @PostMapping
-    @CrossOrigin
-    @RequestMapping("/salvar")
-    public ResponseEntity<Produto> adicionarProduto(@RequestBody Produto produto) {
-        produtoRepository.save(produto);
-        return ResponseEntity.ok().body(produto);
-    }
-
     @GetMapping
     @CrossOrigin
     @RequestMapping("/")
@@ -37,4 +31,41 @@ public class ProdutoController {
         List<Produto> listaProdutos = produtoRepository.findAll();
         return listaProdutos;
     }
+
+    @CrossOrigin
+    @RequestMapping("/buscar")
+    public ResponseEntity<Produto> buscarProdutoById(@RequestParam Long id) {
+        Produto produto = produtoRepository.findById(id).get();
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @PostMapping
+    @CrossOrigin
+    @RequestMapping("/salvar")
+    public ResponseEntity<Produto> adicionarProduto(@RequestBody Produto produto) {
+ /*       if (produto.getId() != null) {
+            produtoRepository.deleteById(produto.getId());
+        }
+        Produto p = new Produto();
+        p.setNome(produto.getNome());
+        p.setQuantidade(produto.getQuantidade());
+*/
+        produtoRepository.save(produto);
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @CrossOrigin
+    @RequestMapping("/apagar")
+    public ResponseEntity<String> apagarProdutoById(@RequestParam Long id) {
+        String resp;
+        try {
+            produtoRepository.deleteById(id);
+            resp = "Apagado com sucesso!";
+        } catch (Exception erro) {
+            System.out.println("--------------------------> ERROR " + erro);
+            resp = "Erro ao apagar";
+        }
+        return ResponseEntity.ok().body(resp);
+    }
+
 }
